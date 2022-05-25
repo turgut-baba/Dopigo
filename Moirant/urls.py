bb604e049aef357964a0480fc0e06e17ac101caa
turgut-baba <56086820+turgut-baba@users.noreply.github.com>
2022-05-25 22:07:52 +0300

    Update urls.py

--- Moirant/urls.py
+++ Moirant/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('stocks.urls')),
-    path('accounts/', include('accounts.urls')),
     path('stores/', include('stores.urls')),
 ]
